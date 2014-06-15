8cb8cb2108e7a4f24f81033e24459c2a57c18a3b
Gabo <gabo@moncarz.com.ar>
2014-06-15 15:13:51 -0300

    improving discret to only filter ph and alcohol

--- bin/discret.py
+++ bin/discret.py
@@ -8,22 +8,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Discretize tool')
     parser.add_argument('input', help='weka input file')
     parser.add_argument('bins', type=int, help='# bins to discretize')
-    parser.add_argument('method', choices=('frecuencia', 'densidad', 'supervisado'), 
+    parser.add_argument('method', choices=('frecuencia', 'ancho', 'supervisado'), 
       help='# bins to discretize')
     args = parser.parse_args()
 
-    if args.method == 'frecuencia':
-      cmd = r'java -Xmx256m -classpath /usr/share/java/weka.jar weka.filters.unsupervised.attribute.Discretize -B %d < %s' \
+    if args.method == 'ancho':
+      cmd = r'java -Xmx256m -classpath /usr/share/java/weka.jar weka.filters.unsupervised.attribute.Discretize -B %d -R 10,12 < %s' \
         % (args.bins, args.input)
-    elif args.method == 'densidad':
-      cmd = r'java -Xmx256m -classpath /usr/share/java/weka.jar weka.filters.unsupervised.attribute.Discretize -B %d -F < %s' \
+    elif args.method == 'frecuencia':
+      cmd = r'java -Xmx256m -classpath /usr/share/java/weka.jar weka.filters.unsupervised.attribute.Discretize -B %d -F -R 10,12 < %s' \
         % (args.bins, args.input)
     else:
-      cmd = r'java -Xmx256m -classpath /usr/share/java/weka.jar weka.filters.supervised.attribute.Discretize -c last < %s' \
+      cmd = r'java -Xmx256m -classpath /usr/share/java/weka.jar weka.filters.supervised.attribute.Discretize -c last -R 10,12 < %s' \
         % (args.input)
         
-        
-
     os.system(cmd)
 
 # vim: set expandtab ts=4 sw=4:
